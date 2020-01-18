0cdb93a899e7c28d4540f70c348ac5b1e1e469e0
Leonard Seibold <leo@zrtx.de>
2020-01-18 21:51:35 +0100

    Basic structure

--- minecraft-filter.c
+++ minecraft-filter.c
@@ -1,13 +1,44 @@
+#include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/netfilter.h>
+#include <linux/netfilter_ipv4.h>
+#include <linux/ip.h>
+#include <linux/tcp.h>
+#include <linux/udp.h>
 
-int init_module(void)
+static struct nf_hook_ops *nfho = NULL;
+
+unsigned int hook_func(
+        void *priv,
+        struct sk_buff *skb,
+        const struct nf_hook_state *state)
+{
+    printk(KERN_INFO "Packet dropped\n");
+    return NF_DROP;
+}
+
+static int __init LKM_init(void)
 {
     printk(KERN_INFO "Loading minecraft netfilter kernel module!\n");
+
+    nfho = (struct nf_hook_ops*) kcalloc(1, sizeof(struct nf_hook_ops), GFP_KERNEL);
+
+    nfho->hook = (nf_hookfn*)hook_func;
+    nfho->hooknum = NF_INET_PRE_ROUTING;
+    nfho->pf = PF_INET;
+    nfho->priority = NF_IP_PRI_FIRST;
+
+    nf_register_net_hook(&init_net, nfho);
     return 0;
 }
 
-void cleanup_module(void)
+static void __exit LKM_exit(void)
 {
     printk(KERN_INFO "Cleaning up minecraft netfilter kernel module...\n");
+    nf_unregister_net_hook(&init_net, nfho);
+    kfree(nfho);
 }
+
+module_init(LKM_init);
+module_exit(LKM_exit);
