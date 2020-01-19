ae27b99d7baf98725696f6c2d018f5a596e300f4
Leonard Seibold <leo@zrtx.de>
2020-01-19 01:24:14 +0100

    stuff

--- minecraft-filter.c
+++ minecraft-filter.c
@@ -4,6 +4,7 @@
 #include <linux/netfilter.h>
 #include <linux/netfilter_ipv4.h>
 #include <linux/ip.h>
+#include <net/ip.h>
 #include <linux/tcp.h>
 #include <linux/udp.h>
 
@@ -14,8 +15,33 @@ unsigned int hook_func(
         struct sk_buff *skb,
         const struct nf_hook_state *state)
 {
-    printk(KERN_INFO "Packet dropped\n");
-    return NF_DROP;
+    struct iphdr *iph;
+    struct tcphdr *tcph;
+
+    iph = ip_hdr(skb);
+    if (iph->protocol == IPPROTO_TCP) {
+        tcph = tcp_hdr(skb);
+        if (ntohs(tcph->dest) == 25565 && !tcph->syn && !tcph->fin && !tcph->rst) {
+            int tcph_len = tcph->doff*4;
+            unsigned char* payload = (unsigned char *)((unsigned char *)tcph + tcph_len);
+            int payload_size = ntohs(iph->tot_len)-ip_hdrlen(skb)-tcph_len;
+            
+            unsigned char* payload_copy = kcalloc(payload_size*20+1, sizeof(unsigned char), GFP_KERNEL);
+            unsigned char* c_pointer = payload_copy;
+            int i;
+            for (i = 0; i < payload_size; i++) {
+                sprintf(c_pointer, "0x%02x ", payload[i]);
+                c_pointer += 5;
+            }
+
+            printk(KERN_INFO "MC PACKET: tot_len=%d payload_size=%d ip_hdrlen=%d tcph_len=%d data=%s\n", ntohs(iph->tot_len), payload_size, ip_hdrlen(skb), tcph_len,  payload_copy);
+            kfree(payload_copy);
+            return NF_DROP;
+        }
+
+    }
+    
+    return NF_ACCEPT;
 }
 
 static int __init LKM_init(void)
